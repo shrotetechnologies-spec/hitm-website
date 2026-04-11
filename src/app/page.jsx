9ab567bcd0ca63e19cd6677b89a317d4857ef0be
Alok-eev <alok.eevv@gmail.com>
2026-04-12 00:50:23 +0530

    ggc

--- src/app/page.jsx
+++ src/app/page.jsx
@@ -421,6 +421,7 @@ export default function HomePage() {
         </div>
       </section>
 
+
       <section className="py-20 bg-gray-950">
         <div className="container mx-auto px-4">
           <div className="section-title">
